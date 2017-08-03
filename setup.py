dcd63a630d467b1564a2d444d743d76e556b5fe3
March <m2.march@gmail.com>
2017-08-03 11:13:29 -0300

    Beatroot module for use outside and inside python

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from subprocess import Popen
+
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    def run(self):
+        Popen(['make', '-C', 'm2/beatroot'])
+        install.run(self)
+
+class PostDevelopCommand(develop):
+    def run(self):
+        Popen(['make', '-C', 'm2/beatroot'])
+        develop.run(self)
+
+setup(name='beatroot',
+      version='0.1',
+      description='API for beatroot for use from python',
+      author='Martin "March" Miguel',
+      author_email='m2.march@gmail.com',
+      packages=['m2.beatroot'],
+      namespace_packages=['m2'],
+      scripts=['scripts/beatroot', 'scripts/beatroot_onsets'],
+      package_data={
+          'm2.beatroot': ['m2/beatroot/BeatRoot.jar']
+      },
+      cmdclass={
+          'develop': PostDevelopCommand,
+          'install': PostInstallCommand
+      }
+)
