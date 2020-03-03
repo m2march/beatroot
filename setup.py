c714b2454a15c75a6e7b98e4e97ef1169fc0b42b
March <m2.march@gmail.com>
2020-03-03 20:40:48 -0300

    Properly finding packages

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools.command.install import install
 class PostInstallCommand(install):
     def run(self):
         Popen(['make', '-C', 'beatroot'])
+        install.do_egg_install(self)
         install.run(self)
 
 class PostDevelopCommand(develop):
@@ -22,14 +23,18 @@ setup(name='beatroot',
       description='API for beatroot for use from python',
       author='Martin "March" Miguel',
       author_email='m2.march@gmail.com',
-      packages=['m2.beatroot'],
+      packages=['m2', 'm2.beatroot'],
       namespace_packages=['m2'],
       scripts=['scripts/beatroot'],
       package_data={
-          'm2.beatroot': ['BeatRoot.jar']
+          'm2.beatroot': ['BeatRoot.jar', 'beatroot-0.5.8.jar']
       },
       cmdclass={
           'develop': PostDevelopCommand,
           'install': PostInstallCommand
-      }
+      },
+      install_requires=[
+          'numpy',
+          'scipy'
+      ]
 )
